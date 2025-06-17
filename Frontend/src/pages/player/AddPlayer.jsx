d0e44ae46e216275278b95cb794a444a740073d3
Abhinaba Das <dasabhi1910@gmail.com>
2025-06-17 11:10:06 +0530

    api.js fix

--- Frontend/src/pages/player/AddPlayer.jsx
+++ Frontend/src/pages/player/AddPlayer.jsx
@@ -188,7 +188,7 @@ export default function AddPlayer() {
                     <option value="">Select role...</option>
                     {[
                       "Batsman",
-                      "Fast all-rounde",
+                      "Fast all-rounder",
                       "Spin all-rounder",
                       "Wicket keeper batsman",
                       "Spin bowler",
