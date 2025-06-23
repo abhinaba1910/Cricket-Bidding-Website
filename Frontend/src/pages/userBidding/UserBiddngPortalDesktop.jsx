20136795d8062eda35f501213087423a62031b21
Vinay reddy koukuntla <142045389+vny-rdy@users.noreply.github.com>
2025-06-23 22:27:09 +0530

    added frontend

--- Frontend/src/pages/userBidding/UserBiddngPortalDesktop.jsx
+++ Frontend/src/pages/userBidding/UserBiddngPortalDesktop.jsx
@@ -608,7 +608,6 @@ export default function UserBiddingDashboardDesktop() {
             </p>
           </motion.div>
         ))}
-
         {/* ─ Current Player Details ─ */}
         <motion.div
           onClick={openPlayerModal}
