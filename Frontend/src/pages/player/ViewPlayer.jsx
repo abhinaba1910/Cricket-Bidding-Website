ec50fe2b09d670f95327eab2eee3588d1f036cd9
Abhinaba Das <dasabhi1910@gmail.com>
2025-06-24 17:10:23 +0530

    made changes in RTM things and all the things are fixed

--- Frontend/src/pages/player/ViewPlayer.jsx
+++ Frontend/src/pages/player/ViewPlayer.jsx
@@ -172,7 +172,7 @@ export default function ViewPlayer() {
                   icon={<FiDollarSign />}
                 />
                 <StatItem 
-                  title="Points" 
+                  title="Rating" 
                   value={points} 
                   icon={<FiAward />}
                 />
