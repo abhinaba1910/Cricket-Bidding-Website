6b8ce462653ebfc1f0cb790b4217dae74554c433
Abhinaba Das <dasabhi1910@gmail.com>
2025-07-09 14:25:15 +0530

    update of timer and forgot password and celebration animation implemented by abhi

--- Backend/server.js
+++ Backend/server.js
@@ -132,6 +132,10 @@ app.use(cors({
 }));
 app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ extended: true, limit: "1mb" }));
+// Express example
+app.get("/", (req, res) => {
+  res.status(200).send("OK");
+});
 
 // ── MongoDB Connect ───────────────────────────────
 mongoose.connect(process.env.MONGO_URI || "mongodb+srv://dasabhi1910:iamf00L@cricket-bidding.dugejrq.mongodb.net/", {
